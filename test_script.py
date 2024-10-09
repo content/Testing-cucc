de22094f8db72886d428947315ad007d97a7a130
Tigi <61912726+content@users.noreply.github.com>
2024-10-09 11:33:43 +0200

    Legjobb tesztelésekben :-D

--- test_script.py
+++ test_script.py
@@ -12,3 +12,15 @@ def test_calculateDifference():
     assert calculateDifference(0, 0) == 0
     assert calculateDifference(-1, 1) == -2
     assert calculateDifference(-1, -1) == 0
+    
+def test_calculateProduct():
+    assert calculateProduct(2, 3) == 6
+    assert calculateProduct(0, 0) == 0
+    assert calculateProduct(-1, 1) == -1
+    assert calculateProduct(-1, -1) == 1
+    
+def test_calculateQoutient():
+    assert calculateQuotient(6, 3) == 2
+    assert calculateQuotient(0, 1) == 0
+    assert calculateQuotient(-1, 1) == -1
+    assert calculateQuotient(-1, -1) == 1
