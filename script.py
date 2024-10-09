314fad9aac31cd4d166c15c76e479c3cd7499dc2
Tigi <61912726+content@users.noreply.github.com>
2024-10-09 10:40:47 +0200

    Updated script.py

--- script.py
+++ script.py
@@ -1,11 +1,26 @@
-def calculateSum(a, b)
+import pygame
+
+def calculateSum(a, b):
     return a + b
 
-def calculateDifference(a, b)
+def calculateDifference(a, b):
     return a - b
 
-def calculateProduct(a, b)
+def calculateProduct(a, b):
     return a * b
 
-def calculateQuotient(a, b)
+def calculateQuotient(a, b):
     return a / b
+
+# Create a calculator project
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+operation = input("Enter operation: ")
+if(operation == '+'):
+    print(calculateSum(num1, num2))
+elif(operation == '-'):
+    print(calculateDifference(num1, num2))
+elif(operation == '*'):
+    print(calculateProduct(num1, num2))
+elif(operation == '/'):
+    print(calculateQuotient(num1, num2))
