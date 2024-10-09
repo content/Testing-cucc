f4a67ca1424dd6f192d0089042986f0a6acbb100
Tigi <61912726+content@users.noreply.github.com>
2024-10-09 10:27:31 +0200

    Initalize repositry

---
+++ script.py
@@ -0,0 +1,11 @@
+def calculateSum(a, b)
+    return a + b
+
+def calculateDifference(a, b)
+    return a - b
+
+def calculateProduct(a, b)
+    return a * b
+
+def calculateQuotient(a, b)
+    return a / b
